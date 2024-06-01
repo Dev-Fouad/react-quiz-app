35a93e782e48ae4a22d471f42644d4004c54b2c5
Fouad damisi <fouadcodes@gmail.com>
2024-06-01 23:41:57 +0100

    "Refactored Quizpage component to improve state management and error handling during question fetching"

--- src/pages/app/Quizpage.jsx
+++ src/pages/app/Quizpage.jsx
@@ -1,33 +1,43 @@
 import React, { useEffect, useState } from "react";
 
 function Quizpage() {
-  // uSE STATE  hook
+  // State hooks for managing loading status, error messages, and questions data
   const [isloading, setisLoading] = useState(false);
   const [error, setError] = useState("");
   const [questions, setQuestions] = useState([]);
 
-  // use Effect hook
+  // useEffect hook for fetching questions when the component mounts
   useEffect(() => {
     async function fetchQuestions() {
       try {
+        // Set loading state to true before starting the fetch operation
         setisLoading(true);
+
+        // Fetch questions from the API
         const res = await fetch(
           "https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean"
         );
 
+        // Throw an error if the response is not ok
         if (!res.ok)
           throw new Error("Something went wrong with fetching questions");
 
+        // Parse the JSON data from the response
         const data = await res.json();
-        setQuestions(data);
+
+        // Set the questions state with the fetched data
+        setQuestions(data.results);
       } catch (err) {
+        // Log the error and set the error state
         console.err(err.message);
         setError(err.message);
       } finally {
+        // Set loading state to false after the fetch operation completes
         setisLoading(false);
       }
     }
 
+    // Call the fetchQuestions function
     fetchQuestions();
   }, []);
 
