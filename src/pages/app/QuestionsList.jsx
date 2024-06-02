e330fdd2408dfd1006d6b34dfaabf9ff42e41afe
Fouad damisi <fouadcodes@gmail.com>
2024-06-02 16:34:30 +0100

    "Removed question text and reformatted question count display"

--- src/pages/app/QuestionsList.jsx
+++ src/pages/app/QuestionsList.jsx
@@ -16,11 +16,7 @@ function QuestionsList({ questions }) {
         {questions[currentQuestionIndex].category}
       </h2>
 
-      <div className="border-2  w-[70%] border-black p-8 ">
-        {` Question ${currentQuestionIndex + 1} : ${
-          questions[currentQuestionIndex].question
-        } `}
-      </div>
+      <div className="border-2  w-[70%] border-black p-8 "></div>
 
       <div className="flex space-x-6 justify-center pt-[50px]">
         <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
@@ -33,10 +29,9 @@ function QuestionsList({ questions }) {
         </button>
       </div>
 
-      <h2 className="text-center text-[25px] pt-[50px]">{`${
-        currentQuestionIndex + 1
-        // eslint-disable-next-line react/prop-types
-      } of ${questions.length}`}</h2>
+      <h2 className="text-center text-[25px] pt-[50px]">
+        {currentQuestionIndex + 1} of {questions.length}
+      </h2>
     </div>
   );
 }
