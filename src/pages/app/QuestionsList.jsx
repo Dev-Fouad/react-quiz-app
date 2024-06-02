4ebe5128e3306e613278dd27c331f97be2efb073
Fouad damisi <fouadcodes@gmail.com>
2024-06-02 17:42:53 +0100

    "Refactored QuestionsList component and added questions data to Routers component"

--- src/pages/app/QuestionsList.jsx
+++ src/pages/app/QuestionsList.jsx
@@ -1,32 +1,47 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-// eslint-disable-next-line react/prop-types,
+// eslint-disable-next-line react/prop-types
 function QuestionsList({ questions }) {
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(9);
-  // const [userAnswers, setUserAnswers] = useState([]);
-  // const [quizCompleted, setQuizCompleted] = useState(false);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const navigate = useNavigate();
 
-  console.log(questions[currentQuestionIndex].question);
-  // eslint-disable-next-line react/prop-types
-  // console.log(questions[currentQuestionIndex].category);
+  const handleAnswerClick = (answer) => {
+    setUserAnswers([...userAnswers, answer]);
+    if (currentQuestionIndex + 1 < questions.length) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    } else {
+      navigate("/results", { state: { questions, userAnswers } });
+    }
+  };
 
+  const currentQuestion = questions[currentQuestionIndex];
   return (
     <div className="font-mono flex flex-col items-center justify-center py-[50px] ">
       <h2 className=" text-[30px] text-black max-md:text-[25px] font-mono text-center font-bold">
         {questions[currentQuestionIndex].category}
       </h2>
 
-      <div className="border-2  w-[70%] border-black p-8 "></div>
-
-      <div className="flex space-x-6 justify-center pt-[50px]">
-        <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-          {" "}
-          True{" "}
-        </button>
-        <button className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600">
-          {" "}
-          False{" "}
-        </button>
+      <div className="border-2  w-[70%] border-black p-8 mt-[20px] ">
+        Question {currentQuestionIndex + 1}:{" "}
+        {questions[currentQuestionIndex].question}
+        <div className="flex space-x-6 justify-center pt-[30px]">
+          <button
+            onClick={() => handleAnswerClick("True")}
+            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          >
+            {" "}
+            True{" "}
+          </button>
+          <button
+            onClick={() => handleAnswerClick("False")}
+            className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
+          >
+            {" "}
+            False{" "}
+          </button>
+        </div>
       </div>
 
       <h2 className="text-center text-[25px] pt-[50px]">
--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -1,13 +1,148 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "../pages/app/Homepage";
-import Quizpage from "../pages/app/Quizpage";
+// import Quizpage from "../pages/app/Quizpage";
+import QuestionsList from "../pages/app/QuestionsList";
 
 function Routers() {
+  const questions = [
+    {
+      type: "boolean",
+      difficulty: "hard",
+      category: "History",
+      question: "The man that shot Alexander Hamilton was named Aaron Burr.",
+      correct_answer: "True",
+      incorrect_answers: ["False"],
+    },
+
+    {
+      type: "boolean",
+      difficulty: "hard",
+      category: "Entertainment: Video Games",
+      question:
+        "In The Witcher 3, the Zoltan Chivay Gwent card can be found under the Hanged Man&#039;s Tree.",
+      correct_answer: "True",
+      incorrect_answers: ["False"],
+    },
+
+    {
+      type: "boolean",
+      difficulty: "hard",
+      category: "Geography",
+      question:
+        "The two largest ethnic groups of Belgium are Flemish and Walloon. ",
+      correct_answer: "True",
+      incorrect_answers: ["False"],
+    },
+
+    {
+      type: "boolean",
+
+      difficulty: "hard",
+
+      category: "Entertainment: Video Games",
+
+      question: "Unturned originally started as a Roblox game.",
+
+      correct_answer: "True",
+
+      incorrect_answers: ["False"],
+    },
+
+    {
+      type: "boolean",
+
+      difficulty: "hard",
+
+      category: "Science: Mathematics",
+
+      question:
+        "L&#039;H&ocirc;pital was the mathematician who created the homonymous rule that uses derivatives to evaluate limits with indeterminations.",
+
+      correct_answer: "False",
+
+      incorrect_answers: ["True"],
+    },
+
+    {
+      type: "boolean",
+
+      difficulty: "hard",
+
+      category: "Science: Mathematics",
+
+      question:
+        "If you could fold a piece of paper in half 50 times, its&#039; thickness will be 3/4th the distance from the Earth to the Sun.",
+
+      correct_answer: "True",
+
+      incorrect_answers: ["False"],
+    },
+
+    {
+      type: "boolean",
+
+      difficulty: "hard",
+
+      category: "Entertainment: Video Games",
+
+      question:
+        "In &quot;Portal 2&quot;, Cave Johnson started out Aperture Science as a shower curtain company.",
+
+      correct_answer: "True",
+
+      incorrect_answers: ["False"],
+    },
+
+    {
+      type: "boolean",
+
+      difficulty: "hard",
+
+      category: "Entertainment: Japanese Anime &amp; Manga",
+
+      question:
+        "In the &quot;To Love-Ru&quot; series, Peke is considered a female robot.",
+
+      correct_answer: "True",
+
+      incorrect_answers: ["False"],
+    },
+
+    {
+      type: "boolean",
+
+      difficulty: "hard",
+
+      category: "General Knowledge",
+
+      question:
+        "&quot;Number 16 Bus Shelter&quot; was a child&#039;s name that was approved by the New Zealand government.",
+
+      correct_answer: "True",
+
+      incorrect_answers: ["False"],
+    },
+
+    {
+      type: "boolean",
+
+      difficulty: "hard",
+
+      category: "History",
+
+      question:
+        "The fourth funnel of the RMS Titanic was fake designed to make the ship look more powerful and symmetrical.",
+
+      correct_answer: "True",
+
+      incorrect_answers: ["False"],
+    },
+  ];
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/quiz" element={ <Quizpage />} />
+        <Route path="/quiz" element={<QuestionsList questions={questions} />} />
       </Routes>
     </Router>
   );
