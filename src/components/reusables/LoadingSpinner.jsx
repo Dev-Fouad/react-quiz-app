76cce65dd8bad8d3ef67ddb68b9a289b64fd754d
Fouad damisi <fouadcodes@gmail.com>
2024-06-04 00:26:55 +0100

    "Update LoadingSpinner component to use 'min-h-svh' instead of 'h-screen' in CSS class"

--- src/components/reusables/LoadingSpinner.jsx
+++ src/components/reusables/LoadingSpinner.jsx
@@ -2,7 +2,7 @@ import "./Loading.css";
 
 const LoadingSpinner = () => {
   return (
-    <div className="justify-center items-center flex h-screen">
+    <div className="justify-center items-center flex min-h-svh">
       <div className="lds-roller">
         <div></div>
         <div></div>
