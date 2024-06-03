d1e0641ad75b811cea702be93f0084561cbe1bce
Fouad damisi <fouadcodes@gmail.com>
2024-06-03 10:16:30 +0100

    "Updated Homepage component: changed 'h-[100%]' to 'h-screen' and added line breaks to Link component"

--- src/pages/app/Homepage.jsx
+++ src/pages/app/Homepage.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Homepage() {
   return (
-    <div className="flex px-[20px] flex-col items-center justify-center h-[100%] bg-gradient-to-r from-green-400 via-blue-500 to-purple-600">
+    <div className="flex px-[20px] flex-col items-center justify-center h-screen bg-gradient-to-r from-green-400 via-blue-500 to-purple-600">
       <div className="bg-white shadow-lg rounded-lg p-10 max-sm:p-[20px] max-w-lg w-full">
         <h1 className="text-4xl font-bold mb-6 text-center font-mono text-gray-800">
           Welcome to the Trivia Challenge!
@@ -14,7 +14,8 @@ function Homepage() {
           Can you score 100%?
         </p>
         <div className="flex justify-center">
-          <Link to="/quiz"
+          <Link
+            to="/quiz"
             className="px-8 py-4 bg-blue-600 text-white text-lg rounded-full shadow-lg hover:bg-blue-700 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110"
           >
             BEGIN
